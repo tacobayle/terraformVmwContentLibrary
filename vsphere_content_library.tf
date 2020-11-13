f0d8ab9291843f58deca504176f949be415b8ccb
Nicolas Bayle <nicolas.bayle.it@gmail.com>
2020-11-13 11:51:03 +0000

    20201113

--- vsphere_content_library.tf
+++ vsphere_content_library.tf
@@ -3,3 +3,10 @@ resource "vsphere_content_library" "library" {
   storage_backing = [data.vsphere_datastore.datastore.id]
   description     = "A new source of content"
 }
+
+resource "vsphere_content_library_item" "aviController" {
+  name        = "Avi Controller 20.1.1"
+  description = "Avi Controller 20.1.1"
+  library_id  = vsphere_content_library.library.id
+  file_url = "controller-20.1.1-9071.ova"
+}
